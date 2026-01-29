d6c4bd6a6bfd7f216d266de9957e70a3eab0914b
CharisLai <103364394+CharisLai@users.noreply.github.com>
2026-01-29 13:54:35 +0800

    fix: vite.config.js error

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ?
+  base: process.env.NODE_ENV === "production"
     ? '/Week2_Vite/'
     : '/',
   plugins: [react()],
